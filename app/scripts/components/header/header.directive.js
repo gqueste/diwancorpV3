479e017c4e9850f60174e4dc5ef34ac5ac79b0c6
gabriel.queste@gmail.com <gabriel.queste@gmail.com>
2015-07-20 14:56:35 +0100

    Passage du Header en Directive

---
+++ app/scripts/components/header/header.directive.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  function diwancorpHeader(){
+    return{
+      restrict: 'E',
+      templateUrl: 'scripts/components/header/header.html',
+      controller: 'HeaderCtrl'
+    }
+  }
+
+
+  angular
+    .module('diwancorpApp')
+    .directive('diwancorpHeader', diwancorpHeader);
+
+  SagaCtrl.$inject = [];
+})();
